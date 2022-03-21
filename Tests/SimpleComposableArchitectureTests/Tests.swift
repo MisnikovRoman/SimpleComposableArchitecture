9afa297c26afd9466b93acdc07361410ee47ea2d
Misnikov Roman <misnikovroman@gmail.com>
2022-03-21 12:00:06 +0300

    Fixes framework build

--- Tests/SimpleComposableArchitectureTests/Tests.swift
+++ Tests/SimpleComposableArchitectureTests/Tests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import simple_composable_architecture
+@testable import SimpleComposableArchitecture
 
-final class simple_composable_architectureTests: XCTestCase {
+final class SimpleComposableArchitectureTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
